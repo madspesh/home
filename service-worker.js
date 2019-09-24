2f6ef29299159f2225f830bbf610e89854c0dbfe
Rebecca <madspesh@gmail.com>
2019-09-24 03:06:15 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/madspesh_2/precache-manifest.49cc8b49094e814ef100c8682e12a579.js"
+  "/madspesh_2/precache-manifest.dd929f2a3c749d8b821cfcfa4b5c180c.js"
 );
 
 self.addEventListener('message', (event) => {
